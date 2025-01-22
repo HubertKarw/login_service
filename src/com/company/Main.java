b8f75c46e38c6ec809f23d912b652ee6e21da7b1
Hubert.Karw <hubert_karwowski@o2.pl>
2025-01-22 19:38:14 +0100

    TASK4 4 proba 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("1 proba taska 4");
         System.out.println("2 proba taska 4");
         System.out.println("3 proba taska 4");
-
+        System.out.println("4 proba taska 4");
 
     }
 }
