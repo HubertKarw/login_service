4803ee750dc081515b546b6d8940d65273f4424d
HubertKarw <98644401+HubertKarw@users.noreply.github.com>
2025-01-22 19:46:47 +0100

    Feature/task4 (#4)
    TASK4 squash of commits of TASK4, final version of algorithm, testing of A1, A2 ,A3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,4 +6,12 @@ public class Main {
         System.out.println("task2 completed");
         System.out.println("Task 3");
     }
+    public static void algorithmTask4(){
+        System.out.println("1 proba taska 4");
+        System.out.println("2 proba taska 4");
+        System.out.println("3 proba taska 4");
+        System.out.println("4 proba taska 4");
+        System.out.println("5 proba taska 4");
+
+    }
 }
